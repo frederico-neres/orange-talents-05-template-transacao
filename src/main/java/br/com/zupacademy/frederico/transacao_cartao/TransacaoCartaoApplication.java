d22bfc749268b070d230d00aafabf7df4473e001
Frederico Neres <neresfrederico@gmail.com>
2021-06-18 08:37:47 -0300

    setup do projeto

---
+++ src/main/java/br/com/zupacademy/frederico/transacao_cartao/TransacaoCartaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.frederico.transacao_cartao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaoCartaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacaoCartaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupacademy/frederico/transacao_cartao/TransacaoCartaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.frederico.transacao_cartao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoCartaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
